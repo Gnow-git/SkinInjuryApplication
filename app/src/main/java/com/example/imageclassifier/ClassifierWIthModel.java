8a7bde96c995e6d30bbe5f0db3fe975247bdfa69
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2022-08-01 15:56:30 +0900

    Classifier Implementation

--- app/src/main/java/com/example/imageclassifier/ClassifierWIthModel.java
+++ app/src/main/java/com/example/imageclassifier/ClassifierWIthModel.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Pair;
 
-import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.Tensor;
 import org.tensorflow.lite.support.common.FileUtil;
 import org.tensorflow.lite.support.common.ops.NormalizeOp;
@@ -14,47 +13,64 @@ import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
 import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.model.Model;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ClassifierWithSupport {
+public class ClassifierWIthModel {
     private static final String MODEL_NAME = "mobilenet_imagenet_model.tflite";
-    private static final String LABEL_FILE = "label.txt";
+    private static final String LABEL_FILE = "labels.txt";
+
     Context context;
-    Interpreter interpreter = null;
+    Model model;
     int modelInputWidth, modelInputHeight, modelInputChannel;
     TensorImage inputImage;
     TensorBuffer outputBuffer;
     private List<String> labels;
 
-    public void init() throws IOException{
-        ByteBuffer model = FileUtil.loadMappedFile(context, MODEL_NAME);
-        model.order(ByteOrder.nativeOrder());
-        interpreter = new Interpreter(model);
+    public void init() throws IOException {
+        model = Model.createModel(context, MODEL_NAME);
 
         initModelShape();
         labels = FileUtil.loadLabels(context, LABEL_FILE);
     }
 
-
-
     private void initModelShape() {
-        Tensor inputTensor = interpreter.getInputTensor(0);
-        int[] inputShape = inputTensor.shape();
-        modelInputChannel = inputShape[0];
-        modelInputWidth = inputShape[1];
-        modelInputHeight = inputShape[2];
+        Tensor inputTensor = model.getInputTensor(0);
+        int[] shape = inputTensor.shape();
+        modelInputChannel = shape[0];
+        modelInputWidth = shape[1];
+        modelInputHeight = shape[2];
 
         inputImage = new TensorImage(inputTensor.dataType());
 
-        Tensor outputTensor = interpreter.getOutputTensor(0);
+        Tensor outputTensor = model.getOutputTensor(0);
 
         outputBuffer = TensorBuffer.createFixedSize(outputTensor.shape(),outputTensor.dataType());
+    }
+
+    public Pair<String, Float> classify(Bitmap image){
+        inputImage = loadImage(image);
+
+        Object[] inputs = new Object[]{inputImage.getBuffer()};
+        Map<Integer, Object> outputs = new HashMap();
+        outputs.put(0, outputBuffer.getBuffer().rewind());
+
+        model.run(inputs, outputs);
+
+        Map<String, Float> output =
+                new TensorLabel(labels, outputBuffer).getMapWithFloatValue();
+
+        return argmax(output);
+    }
+
+    public void finish(){
+        if(model != null)
+            model.close();
     }
 
     private TensorImage loadImage(final Bitmap bitmap) {
@@ -66,16 +82,6 @@ public class ClassifierWithSupport {
                         .add(new NormalizeOp(0.0f, 255.0f))
                         .build();
         return imageProcessor.process(inputImage);
-    }
-
-    public Pair<String, Float> classify(Bitmap image) {
-        inputImage = loadImage(image);
-        interpreter.run(inputImage.getBuffer(), outputBuffer.getBuffer().rewind());
-
-        Map<String, Float> output =
-                new TensorLabel(labels, outputBuffer).getMapWithFloatValue();
-
-        return argmax(output);
     }
 
     private Pair<String, Float> argmax(Map<String, Float> map) {
