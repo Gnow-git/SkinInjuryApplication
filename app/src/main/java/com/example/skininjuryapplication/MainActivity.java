51752fe740e7aa25663ebfbee4e7640e4648c25d
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2022-08-18 16:46:48 +0900

    Add tflite

---
+++ app/src/main/java/com/example/skininjuryapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.skininjuryapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.skininjuryapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button galleryBtn = findViewById(R.id.galleryBtn);
+        galleryBtn.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, GalleryActivity.class);
+            startActivity(i);
+        });
+
+        Button cameraBtn = findViewById(R.id.cameraBtn);
+        cameraBtn.setOnClickListener(view ->{
+            Intent i = new Intent(MainActivity.this, CameraActivity.class);
+            startActivity(i);
+        });
+    }
+}
