f8dfc8bebcba92e77e8d43011f8dbe71f9dfe2e8
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2022-10-18 03:10:11 +0900

    병원검색 구현

--- app/src/main/java/com/example/skininjuryapplication/map/GoogleMapActivity.java
+++ app/src/main/java/com/example/skininjuryapplication/map/GoogleMapActivity.java
@@ -14,8 +14,11 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.skininjuryapplication.R;
+import com.example.skininjuryapplication.user.LoginActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -53,19 +56,33 @@ public class GoogleMapActivity extends AppCompatActivity implements OnMapReadyCa
         System.out.println("이름: " + mapName);
         System.out.println("주소: " + mapAddress);
 
+        // 전달 받은 주소 -> 좌표로 변경
         Geocoder geocoder = new Geocoder(this);
+        List<Address> mResultList = null;
+        String Add = mapAddress;
 
         try {
-            List<Address> mResultList = geocoder.getFromLocationName("충북대학교", 1);
-            double lat = mResultList.get(0).getLatitude();
-            double lon = mResultList.get(0).getLongitude();
-            System.out.println("위 경도" + lat + lon);
+            if(Add != null){
+                mResultList = geocoder.getFromLocationName(Add, 1);
+                if(mResultList.size()!=0){
+                    lat = mResultList.get(0).getLatitude();
+                    lon = mResultList.get(0).getLongitude();
+                    System.out.println("변환 주소:" + mResultList);
+                }
+            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("실패");
+            System.out.println("주소를 불러오지 못했습니다.");
         }
 
-
+        // 지도 검색 버튼 구현
+        Button btn_search = findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(view -> {
+            Intent intent = new Intent(GoogleMapActivity.this, GoogleMapSearchActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
@@ -73,12 +90,23 @@ public class GoogleMapActivity extends AppCompatActivity implements OnMapReadyCa
         // googleMap 마커 지정
         //double lat = 36.625615, lon = 127.454451;
         LatLng location = new LatLng(lat, lon);    // 충북대 소프트웨어
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.title(mapName);
-        markerOptions.snippet(mapAddress);
-        markerOptions.position(location);
-        googleMap.addMarker(markerOptions);
-        // googleMap 확대 설정
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+        LatLng test_location = new LatLng(0.0, 0.0);
+        System.out.println("위도" + lat);
+        System.out.println("경도" + lon);
+        System.out.println("위치" + location);
+        System.out.println("테스트 위치" + test_location);
+
+        if(location.longitude == 0.0 && location.latitude == 0){
+            location = new LatLng(36.625615, 127.454451);
+            Toast.makeText(GoogleMapActivity.this, "지도를 검색해주세요.", Toast.LENGTH_SHORT).show();
+        }
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.title(mapName);
+            markerOptions.snippet(mapAddress);
+            markerOptions.position(location);
+            googleMap.addMarker(markerOptions);
+            // googleMap 확대 설정
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+
     }
 }
